b51764300a7c7464103d9d0a288bfd650213d4f0
alexone <persyak@ukr.net>
2023-09-05 19:43:13 +0300

    Initial commit

---
+++ src/main/java/com/ohorodnik/movieland/MovielandApplication.java
@@ -0,0 +1,13 @@
+package com.ohorodnik.movieland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovielandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovielandApplication.class, args);
+    }
+
+}
