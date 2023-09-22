a5d693740c9193abb923479cade9923c7f9386df
alexone <persyak@ukr.net>
2023-09-22 22:03:15 +0300

    Adding Service tests

--- src/test/java/com/ohorodnik/movieland/service/DefaultMovieServiceTest.java
+++ src/test/java/com/ohorodnik/movieland/service/DefaultMovieServiceTest.java
@@ -11,7 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,16 +40,45 @@ public class DefaultMovieServiceTest {
                 .votes(100)
                 .build();
 
-        Mockito.when(movieRepository.getAllMovies()).thenReturn(List.of(firstMovie));
+        Movie secondMovie = Movie.builder()
+                .id(2)
+                .nameUa("Зелена миля")
+                .nameNative("The Green Mile")
+                .yearOfRelease(LocalDate.of(1999, 1, 1))
+                .description("testDescription2")
+                .rating(9.0)
+                .price(134.67)
+                .picturePath("picturePath2")
+                .votes(100)
+                .build();
+
+        Movie thirdMovie = Movie.builder()
+                .id(3)
+                .nameUa("Форест Гамп")
+                .nameNative("Forrest Gump")
+                .yearOfRelease(LocalDate.of(1994, 1, 1))
+                .description("testDescription3")
+                .rating(8.6)
+                .price(200.60)
+                .picturePath("picturePath3")
+                .votes(100)
+                .build();
+
+        List<Movie> expected = new ArrayList<>();
+        expected.add(firstMovie);
+        expected.add(secondMovie);
+        expected.add(thirdMovie);
+
+        Mockito.when(movieRepository.getAllMovies()).thenReturn(expected);
     }
 
     @Test
     @DisplayName("Get List of One Item on findAllMovies Call")
-    public void whenGetAllMovies_thenReturnListOfOneMovie(){
+    public void whenGetAllMovies_thenReturnListOfThreeMovies(){
         List<Movie> found = movieService.getAllMovies(java.util.Optional.empty(), java.util.Optional.empty());
         Movie actual = found.get(0);
 
-        assertEquals(1, found.size());
+        assertEquals(3, found.size());
         assertEquals(1, actual.getId());
         assertEquals("Втеча з Шоушенка", actual.getNameUa());
         assertEquals("The Shawshank Redemption", actual.getNameNative());
@@ -56,6 +87,24 @@ public class DefaultMovieServiceTest {
         assertEquals(8.9, actual.getRating());
         assertEquals(123.45, actual.getPrice());
         assertEquals("picturePath1", actual.getPicturePath());
+        assertEquals(100, actual.getVotes());
+    }
+
+    @Test
+    @DisplayName("Return list of movies sorted by rating in desc when requested")
+    public void whenGetAllMoviesWithRatingDesc_thenReturnSortedListByRatingDesc(){
+        List<Movie> found = movieService.getAllMovies(Optional.of("desc"), Optional.empty());
+        Movie actual = found.get(0);
+
+        assertEquals(3, found.size());
+        assertEquals(2, actual.getId());
+        assertEquals("Зелена миля", actual.getNameUa());
+        assertEquals("The Green Mile", actual.getNameNative());
+        assertEquals(LocalDate.of(1999, 1, 1), actual.getYearOfRelease());
+        assertEquals("testDescription2", actual.getDescription());
+        assertEquals(9.0, actual.getRating());
+        assertEquals(134.67, actual.getPrice());
+        assertEquals("picturePath2", actual.getPicturePath());
         assertEquals(100, actual.getVotes());
     }
 }
